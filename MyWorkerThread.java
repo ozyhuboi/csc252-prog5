7d04db5b30e1876d052b3db3c301fade119b695c
Yibo Zhou <yzhou22@u.rochester.edu>
2015-04-26 18:02:00 -0400

    MyProxyServer and Thread
    Can recognize and loop around for sockets.

---
+++ MyWorkerThread.java
@@ -0,0 +1,20 @@
+/*
+ * MyWorkerThread consists of the main thread of functions used and called back in the main 
+ */
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class MyWorkerThread {
+	
+	public MyWorkerThread (Socket sock) {
+		
+	}
+
+	public void start() {
+		
+		
+	}
+
+}
